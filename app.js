c9d7ba1197fafa7a289fecce775d19c54d4ff0bb
qualley <matt.qualley93@gmail.com>
2023-10-19 16:53:29 -0400

    Updating styling

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-import { data } from './modules/data.js';
-import { renderCard, renderLikedCard } from './modules/renderCards.js';
-import { slideUp } from './modules/animations.js';
-
-data.map((post) => {
-    return (post.liked ? renderLikedCard(post) : renderCard(post))
-})
-
-let cards = Array.from(document.querySelectorAll('.card')),
-    title = document.querySelector('.wrapper h1')
-
-slideUp(title, 0, 0)
-
-cards.map((card, index) => {
-    return (index === 0 ? slideUp(card, index, 1.15) : slideUp(card, index, .11))
-})
+window.addEventListener("load", () => {
+    console.log("page is fully loaded");
+    document.querySelectorAll('iframe').forEach( item =>
+        console.log(item.contentWindow.document.body.querySelectorAll('.mobile_download_prompt--overlay--Xsp5l'))
+    )
+  });
